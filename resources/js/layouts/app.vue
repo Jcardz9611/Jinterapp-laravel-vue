5373ff7b0895275f578597ea3c0a49c185be3c0a
José Carlos Arias <jcardz96@gmail.com>
2022-10-24 15:46:51 -0500

    P1.2 Upload image and P2.1 Modal with image

--- resources/js/layouts/app.vue
+++ resources/js/layouts/app.vue
@@ -2,15 +2,30 @@
     <body>
     <div class="gallery">
         <div class="box" v-for="imagen in imagenes" >
-                <img :src="imagen.ruta"  v-b-modal.modal-1>  
+                <img :src="imagen.ruta"  @click="showModal(imagen.ruta)">  
                 <h3>{{imagen.nombre}}</h3>
                 <p>{{imagen.descripcion}}</p> 
         </div>  
-  </b-modal>
+       
     </div>
+
+    <div v-if="modalShow" class="modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-body">
+              <img :src='selectedImgPath'/>
+            </div>
+            <div class="vertical-center">
+                <button @click="hideModal()" type="button">X</button>
+            </div>
+          </div>
+        </div>
+      </div>
+     
     </body>
 </template>
 <script>
+ 
     export default {
         data: function () {
             return {
@@ -32,6 +47,16 @@
                     .catch(function (error) {
                         console.log(error);
                     });
+            },
+            showModal(ruta) {
+                this.modalShow = true;
+                this.selectedImgPath = ruta
+                console.log(ruta)
+                
+               
+            },
+            hideModal() {
+                this.modalShow = false; 
             }
         }
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Jinter App</title>
-    
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
